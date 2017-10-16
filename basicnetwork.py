2ed9364277b95de6ebfb17a4e01a558afba68ac9
14leeyuchieh <14leeyuchieh@berkeley.edu>
2017-10-15 22:59:11 -0700

    added a file of basic network with network structure

---
+++ basicnetwork.py
@@ -0,0 +1,84 @@
+from network import Network
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense, Merge, Add
+from keras.layers.advanced_activations import LeakyReLU
+from keras.optimizers import Adam
+
+
+class basicNetwork(Network):
+	"""
+	A wrapper class for our neural network. This will make it easier
+	to implement the network in different frameworks. All a network needs
+	to do is implement these methods.
+	"""
+	def __init__(self):
+		super(basicNetwork, self).__init__()
+		# put arguments here
+
+	# override
+	def build_network(self, perception_shape, measurements_shape, goals_shape):
+		# perception layer
+		perception_input = Input(shape=perception_shape)
+		perception_conv = Convolution2D(32, 8, strides=4, input_shape=perception_shape)(perception_input)
+		perception_conv = LeakyReLU(alpha=0.2)(perception_conv)
+		perception_conv = Convolution2D(64, 4, strides=2)(perception_input)
+		perception_conv = LeakyReLU(alpha=0.2)(perception_conv)
+		perception_conv = Convolution2D(64, 3, strides=1)(perception_input)
+		perception_conv = LeakyReLU(alpha=0.2)(perception_conv)
+		perception_flattened = Flatten()(perception_conv)
+		perception_fc = Dense(512, activation="linear"))(perception_flattened)
+		# measurement layer
+		measurement_input = Input(shape=measurements_shape)
+		measurement_flatten = Flatten()(measurement_input)
+		measurements_fc = Dense(128)(measurement_flatten)
+		measurements_fc = LeakyReLU(alpha=0.2)(measurements_fc)
+		measurements_fc = Dense(128)(measurements_fc)
+		measurements_fc = LeakyReLU(alpha=0.2)(measurements_fc)
+		measurements_fc = Dense(128, activation="linear")(measurements_fc)
+		# goals layer
+		goal_input = Input(shape=goals_shape)
+		goals_flatten = Flatten()(goal_input)
+		goals_fc = Dense(128)(goals_flatten)
+		goals_fc = LeakyReLU(alpha=0.2)(goals_fc)
+		goals_fc = Dense(128)(goals_fc)
+		goals_fc = LeakyReLU(alpha=0.2)(goals_fc)
+		goals_fc = Dense(128, activation="linear")(goals_fc)
+		# merge together
+		percep = Flatten()(perception_fc)
+		mrment = Flatten()(measurements_fc)
+		goals = Flatten()(goals_fc)
+		mrg_j= Merge(mode='concat')([percep,mrment,goals])
+		#expectations
+		expectation = Dense(512)(mrg_j)
+		expectation = LeakyReLU(alpha=0.2)(expectation)
+		expectation = Dense(3*6, activation="linear")(expectation)
+		#action
+		action = Dense(512)(mrg_j)
+		action = LeakyReLU(alpha=0.2)(action)
+		action = Dense(3*6*256, activation="linear")(action)
+		#concat expectations for number of actions there are
+		expectation = Merge(mode='concat')([expectation]*256)
+		# sum expectations with action
+		expectation_action_sum = Add([action, expectation])
+		adam = Adam(lr=1e-04, beta_1=0.95, beta_2=0.999, epsilon=1e-04, decay=0.3)
+		model.compile(loss='mean_squared_error', optimizer=adam)
+
+	def update_weights(self, exps):
+		# note must change to have it NOT reset learning rate as it currently resets it
+		# also need to double check how exps is structured not sure rn
+		x_train = exps[:len(exps)-1]
+		y_train = exps[len(exps)-1:]
+		# either just put x_train in and let batch be taken care of here or modify this a bit
+		model.fit(x_train, y_train, batch_size=64)
+
+	def loss_func(self, exps):
+		pass
+
+	def predict(self, obs, goal):
+		pass
+	
