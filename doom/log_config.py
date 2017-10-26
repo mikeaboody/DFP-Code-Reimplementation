aa7f006433f04a09f41367de1fc6e1e2158a8638
14leeyuchieh <14leeyuchieh@berkeley.edu>
2017-10-26 16:16:54 -0700

    submit

--- doom/log_config.py
+++ doom/log_config.py
@@ -6,8 +6,8 @@ log_agent_param['testing_num_episodes'] = 5
 log_agent_param['training_num_steps'] = 200
 # this is number of time the function is called
 log_agent_param['test_eval_freq'] = 10
-log_agent_param['step_data_file'] = '/data/agent_step.csv'
-log_agent_param['episode_data_file'] = '/data/agent_episode.csv'
+log_agent_param['step_data_file'] = 'data/agent_step.csv'
+log_agent_param['episode_data_file'] = 'data/agent_episode.csv'
 
 log_agent_param['to_log'] = True
 
