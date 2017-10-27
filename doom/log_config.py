da423dc83900a488f82987d0fb1dd5f1b0273b75
Mike Aboody <mikeaboody@berkeley.edu>
2017-10-27 00:15:50 -0400

    Fix up testing

--- doom/log_config.py
+++ doom/log_config.py
@@ -2,12 +2,11 @@
 # log the agent param
 log_agent_param = {}
 # these are inputted as number of iterations for respective function
-log_agent_param['testing_num_episodes'] = 1
+log_agent_param['testing_num_episodes'] = 2
 log_agent_param['training_num_steps'] = 200
 # this is number of time the function is called
 log_agent_param['test_eval_freq'] = 30
-log_agent_param['step_data_file'] = 'data/agent_step.csv'
-log_agent_param['episode_data_file'] = 'data/agent_episode.csv'
+log_agent_param['test_data_file'] = 'data/agent_test.csv'
 
 log_agent_param['to_log'] = True
 
--- doom/run_exp.py
+++ doom/run_exp.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("..")
 import csv
 import os
+import shutil
 from ai import Agent
 from basic_doom_simulator import create_basic_simulator
 from basicnetwork import basicNetwork_builder
@@ -106,16 +107,58 @@ def test(num_iterations):
 		i += 1
 	doom_simulator.close_game()
 
-def log_config_test():
-	# train a lot
-	train(log_agent_param['training_num_steps'])
-	# test a lot
-	os.makedirs(os.path.dirname(log_agent_param['step_data_file']), exist_ok=True)
-	step_f = open(log_agent_param['step_data_file'],'a')
-	os.makedirs(os.path.dirname(log_agent_param['episode_data_file']), exist_ok=True)
-	ep_f = open(log_agent_param['episode_data_file'],'a')
-	step_writer = csv.writer(step_f)
-	ep_writer = csv.writer(ep_f)
+def run_test(num_episodes, goal, curr_training_iter, agent):
+	"""Returns a size 3 list of the metrics we want: [current # of training iterations for agent,
+		avg of avg health over episodes, avg terminal health over episodes].
+	"""
+	testing_doom_simulator = create_basic_simulator()
+	img = None
+	meas = None
+	terminated = False
+	curr_episode = 0
+	curr_episode_step = 0
+	episode_healths = []
+	#metrics
+	avg_healths = []
+	terminal_healths = []
+
+
+	while curr_episode < num_episodes:
+		if curr_episode_step == 0:
+			action_taken_one_hot = agent.act(goal=goal)
+		else:
+			action_taken_one_hot = agent.act(Observation(img, meas), goal=goal)
+		action_taken = action_from_one_hot(action_taken_one_hot)
+		img, meas, _, terminated = testing_doom_simulator.step(action_taken)
+
+		episode_healths.append(meas[0])
+		curr_episode_step += 1
+		if terminated:
+			#collect metrics
+			avg_health = np.mean(np.array(episode_healths))
+			terminal_health = meas[0]
+			avg_healths.append(avg_health)
+			terminal_healths.append(terminal_health)
+
+			curr_episode += 1
+			curr_episode_step = 0
+			episode_healths = []
+	testing_doom_simulator.close_game()
+
+	return [curr_training_iter, np.mean(np.array(avg_healths)), np.mean(np.array(terminal_healths))]
+
+
+
+def train_and_test():
+	try:
+		shutil.rmtree(os.path.dirname(log_agent_param['test_data_file']))
+	except OSError:
+		pass
+	os.makedirs(os.path.dirname(log_agent_param['test_data_file']))
+	num_episode_test = log_agent_param['testing_num_episodes']
+	num_training_steps = log_agent_param['training_num_steps']
+	freq = log_agent_param['test_eval_freq']
+
 	doom_simulator = create_basic_simulator()
 	goal = np.array([0,0,0,0.5,.5,1])
 	possible_actions = enumerate_action_one_hots(3)
@@ -123,36 +166,27 @@ def log_config_test():
 	img = None
 	meas = None
 	terminated = None
-	step_i = 0
-	episode_healths = []
-	episode_count = 0
-	num_episode_test = log_agent_param['testing_num_episodes']
-	freq = log_agent_param['test_eval_freq']
-	try:
-		while episode_count < num_episode_test:
-			if step_i == 0:
-				action_taken_one_hot = agent.act(goal=goal)
-			else:
-				action_taken_one_hot = agent.act(Observation(img, meas), goal=goal)
-			action_taken = action_from_one_hot(action_taken_one_hot)
-			img, meas, _, terminated = doom_simulator.step(action_taken)
-			if step_i % freq == 0:
-				episode_healths.append(meas[0])
-				step_writer.writerow([meas[0]])
-			if (terminated):
-				ep_writer.writerow([np.mean(episode_healths)])
-				episode_count += 1
-				episode_healths = []
-				agent.signal_episode_end()
-				step_i = 0
-			else:
-				agent.observe(Observation(img, meas), action_taken_one_hot)
-			step_i +=1
-		doom_simulator.close_game()
-	except KeyboardInterrupt:
-		step_f.close()
-		ep_f.close()
-		doom_simulator.close_game()
-		sys.exit()
+	i = 0
+	while i < num_training_steps:
+		if i == 0:
+			action_taken_one_hot = agent.act(training=True)
+		else:
+			action_taken_one_hot = agent.act(Observation(img, meas), training=True)
+		img, meas, _, terminated = doom_simulator.step(action_from_one_hot(action_taken_one_hot))
+		if i % freq == 0:
+			#time to test the agent on real episodes
+			test_data = run_test(num_episode_test, goal, i, agent)
+			with open(log_agent_param['test_data_file'],'a') as ep_f:
+				test_writer = csv.writer(ep_f)
+				test_writer.writerow(test_data)
 
-log_config_test()
+		if (terminated):
+			agent.signal_episode_end()
+		else:
+			agent.observe(Observation(img, meas), action_taken_one_hot)
+		i += 1
+
+	doom_simulator.close_game()
+
+
+train_and_test()
