30f212e0cbd2401fce466655c07e91bb3b577bb4
Mike Aboody <mikeaboody@berkeley.edu>
2017-11-17 16:26:32 -0800

    Rough draft of converting experience to and from array

--- abstraction.py
+++ abstraction.py
@@ -1,4 +1,5 @@
 import numpy as np
+from util import action_to_one_hot
 
 class Observation(object):
     """
@@ -26,11 +27,34 @@ class Experience(object):
         return np.copy(self.obs.sens)
     def meas(self):
         return np.copy(self.obs.meas)
+    def action(self):
+        return np.copy(self.a)
     def goal(self):
         return np.copy(self.g)
     def label(self):
         return np.copy(self.l)
+    def to_array(self):
+        return np.array([self.sens(), self.meas(), self.action(), self.goal(), self.label()])
 
+    @staticmethod
+    def from_array(arr):
+        copy_arr = [np.copy(ele) for ele in arr]
+        sens, meas, a, g, l = copy_arr
+        return Experience(Observation(sens, meas), a, g, l)
 
+def create_experience():
+    sens = np.random.random_sample(size=(84,84,1))
+    meas = np.random.random_sample(size=(1,))
+    goal = np.random.random_sample(size=(6,))
+    # last value indicate index of action
+    label = np.random.random_sample(size=(6,))
+    obs = Observation(sens, meas)
+    exp = Experience(obs, action_to_one_hot([0,1,0]), goal, label)
+    return exp
 
-        
+lst = [create_experience() for _ in range(1000)]
+for exp in lst:
+    copy = Experience.from_array(exp.to_array())
+    same = np.array_equal(exp.sens(), copy.sens()) and np.array_equal(exp.meas(), copy.meas()) and np.array_equal(exp.action(), copy.action()) and np.array_equal(exp.goal(), copy.goal()) and np.array_equal(exp.label(), copy.label())
+    if not same:
+        print("BAD")
