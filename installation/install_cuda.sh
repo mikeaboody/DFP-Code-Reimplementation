a848868e67fd3f87732603f45b72a170ec515f13
Ubuntu <ubuntu@ip-172-31-10-91.us-east-2.compute.internal>
2017-11-21 07:02:00 +0000

    installation

--- installation/install_cuda.sh
+++ installation/install_cuda.sh
@@ -2,3 +2,7 @@ wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/c
 sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda-8-0
+# then set environment variables; ideally add these in ~/.bashrc
+echo "PATH and LD_LIBRARY_PATH set temporarily. Remember to add them to ~/.bashrc."
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} 
+export PATH=/usr/local/cuda/lib64:/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}} 
