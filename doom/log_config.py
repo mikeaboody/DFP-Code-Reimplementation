d33261b514da985fad5d8be869b239afd1c9ae92
Mike Aboody <mikeaboody@berkeley.edu>
2017-11-27 03:36:11 -0800

    First round of cleaning

--- doom/log_config.py
+++ doom/log_config.py
@@ -2,7 +2,7 @@
 # log the agent param
 log_agent_param = {}
 # these are inputted as number of iterations for respective function
-log_agent_param['testing_num_episodes'] = 100
+log_agent_param['testing_num_episodes'] = 1
 log_agent_param['training_num_steps'] = 5000001
 # this is number of time the function is called
 log_agent_param['test_eval_freq'] = 100000
